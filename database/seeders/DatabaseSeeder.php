5641c9875db8a93b6b33206b59349834265fce11
fadiaskeyn <fadiaskeyn@gmail.com>
2025-03-24 02:23:52 +0700

    asdsad

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\DieCertif;
 use App\Models\Ektp;
+use App\Models\FamilyCard;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -18,10 +20,24 @@ class DatabaseSeeder extends Seeder
     {
 
         User::create([
-            'name' => 'user1',
+            'name' => 'user',
             'role' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('123'),
+            'address' => 'Jl, MH. Thamrin'
+        ]);
+        User::create([
+            'name' => 'admin',
+            'role' => 'admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('12'),
+            'password' => Hash::make('123'),
+            'address' => 'Jl, MH. Thamrin'
+        ]);
+        User::create([
+            'name' => 'hanz',
+            'role' => 'admin',
+            'email' => 'hanzslurrr@gmail.com',
+            'password' => Hash::make('12345678'),
             'address' => 'Jl, MH. Thamrin'
         ]);
 
@@ -31,6 +47,19 @@ class DatabaseSeeder extends Seeder
             "user_id" => 1,
             "name" => "Padias",
             "status" => "Diproses"
+        ]);
+        FamilyCard::create([
+            "ktp" => "misalnya_kk.jpg",
+            "maried_certificated" => "misalnya_kk.jpg",
+            "form" => "misalnya_form.jpg",
+            "user_id" => 1
+        ]);
+        DieCertif::create([
+            'form' => 'misalnya_form.jpg',
+            'death_certificate' => 'misalnya_kk.jpg',
+            'kk' => 'misalnya_kk.jpg',
+            'ktp' => 'misalnya_kk.jpg',
+            'user_id' => 1
         ]);
 
 
