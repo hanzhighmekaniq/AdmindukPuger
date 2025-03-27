d6eeccbb0129e3a5872ae2048b289e12df824698
root <root@punyapadias.my.id>
2025-03-27 07:46:59 +0700

    serper

--- resources/views/components/navbar-user.blade.php
+++ resources/views/components/navbar-user.blade.php
@@ -6,7 +6,7 @@
     </a>
 
     <div class="flex md:order-2 space-x-3 md:space-x-0">
-        <a href="{{ route('login') }}"
+        <a href="/login"
             class="poppins-bold text-bold text-white bg-gradient-to-br from-[#5482B4] to-[#021124]  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-8 py-2 text-center">
             Login
         </a>
