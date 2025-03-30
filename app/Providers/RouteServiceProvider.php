09b6317a9c31fbc9071e8dc1e8ea821a9decc473
root <root@punyapadias.my.id>
2025-03-30 18:23:22 +0700

    asd

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,8 +34,6 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
-                
-            
         });
     }
 
